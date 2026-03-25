addffadb88db2716c8f8148f84e75f71261258e0
yun xu <yunxu@yuns-MacBook-Air.local>
2026-03-25 12:15:05 -0400

    add jwt token

---
+++ compass/src/app/model/jwt-info.model.spec.ts
@@ -0,0 +1,7 @@
+import { JwtInfo } from './jwt-infomodel';
+
+describe('JwtInfo', () => {
+  it('should create an instance', () => {
+    expect(new JwtInfo()).toBeTruthy();
+  });
+});
---
+++ compass/src/app/model/jwt-info.model.ts
@@ -0,0 +1,3 @@
+export class JwtInfo {
+    token?:string;
+}
